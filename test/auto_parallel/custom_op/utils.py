4c325cc08dbaa5307723bd41644c993fd847358e
liuzhenhai93 <liuzhenhai93@outlook.com>
2024-01-05 15:12:09 +0000

    custom op use spmd rule

--- test/auto_parallel/custom_op/utils.py
+++ test/auto_parallel/custom_op/utils.py
@@ -15,55 +15,16 @@
 import os
 from site import getsitepackages
 
-import numpy as np
-
 # Test for extra compile args
 extra_cc_args = ['-w', '-g']
 extra_nvcc_args = ['-O3']
 extra_compile_args = {'cc': extra_cc_args, 'nvcc': extra_nvcc_args}
 
 
-def check_output(out, pd_out, name):
-    if out is None and pd_out is None:
-        return
-    assert out is not None, "out value of " + name + " is None"
-    assert pd_out is not None, "pd_out value of " + name + " is None"
-    if isinstance(out, list) and isinstance(pd_out, list):
-        for idx in range(len(out)):
-            np.testing.assert_array_equal(
-                out[idx],
-                pd_out[idx],
-                err_msg='custom op {}: {},\n paddle api {}: {}'.format(
-                    name, out[idx], name, pd_out[idx]
-                ),
-            )
-    else:
-        np.testing.assert_array_equal(
-            out,
-            pd_out,
-            err_msg=f'custom op {name}: {out},\n paddle api {name}: {pd_out}',
-        )
-
-
-def check_output_allclose(out, pd_out, name, rtol=5e-5, atol=1e-2):
-    if out is None and pd_out is None:
-        return
-    assert out is not None, "out value of " + name + " is None"
-    assert pd_out is not None, "pd_out value of " + name + " is None"
-    np.testing.assert_allclose(
-        out,
-        pd_out,
-        rtol,
-        atol,
-        err_msg=f'custom op {name}: {out},\n paddle api {name}: {pd_out}',
-    )
-
-
 def get_paddle_includes():
     env_dict = os.environ
-    paddle_source_dir = env_dict.get("PADDLE_SOURCE_DIR")
     paddle_includes = []
-    paddle_includes.append(f"{paddle_source_dir}")
+    paddle_includes.append(f"{env_dict.get('PADDLE_SOURCE_DIR')}")
 
     # mkldnn
     if env_dict.get("WITH_MKLDNN") == 'ON':
